6fda0ceb4c80ffe9610eeff1b4518c10b5a5e1e1
PranishShresth <yunche64@gmail.com>
2021-08-25 14:14:36 +1000

    game completed (not history)

--- tic-tac-toe/src/GameBoard.js
+++ tic-tac-toe/src/GameBoard.js
@@ -2,13 +2,36 @@ import React, { useState } from "react";
 import Cell from "./Cell";
 
 const initialBoard = Array(9).fill(null);
+
+function checkWinner(board) {
+  const winningPositions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let position of winningPositions) {
+    const [pos1, pos2, pos3] = position;
+    if (board[pos1]) {
+      if (board[pos1] === board[pos2] && board[pos1] === board[pos3]) {
+        return board[pos1];
+      }
+    }
+  }
+  return null;
+}
 export default function GameBoard() {
   const [board, setBoard] = useState(initialBoard);
   const [player1Turn, setPlayer1Turn] = useState(true);
 
   const handleClick = (idx) => {
     const currentBoard = board.slice();
-    if (currentBoard[idx]) {
+    if (currentBoard[idx] || checkWinner(currentBoard)) {
       return;
     }
     currentBoard[idx] = player1Turn ? "🍩" : "🍰";
@@ -29,6 +52,7 @@ export default function GameBoard() {
           );
         })}
       </div>
+      {/* Winner : {checkWinner(board)} */}
     </div>
   );
 }
