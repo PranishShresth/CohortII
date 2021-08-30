bfbca8c39e80f8dd97a055100e3b35ea0ee55cb1
PranishShresth <yunche64@gmail.com>
2021-08-30 15:32:37 +1000

    adding redux thunk

---
+++ redux/src/actions/action.js
@@ -0,0 +1,12 @@
+export async function fetchMyJokes(dispatch) {
+  try {
+    dispatch({ type: "JOKES_FETCH_REQUESTED" });
+    const resp = await fetch(
+      "https://official-joke-api.appspot.com/random_ten"
+    );
+    const data = await resp.json();
+    dispatch({ type: "JOKES_FETCH_SUCCESS", payload: data });
+  } catch (err) {
+    dispatch({ type: "JOKES_FETCH_FAILED", payload: "Fetch jokes failed" });
+  }
+}
--- redux/src/components/Jokes.js
+++ redux/src/components/Jokes.js
@@ -1,24 +1,14 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { fetchMyJokes } from "../actions/action";
 
 export default function Jokes() {
   const jokes = useSelector((state) => state.jokes);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    async function fetchMyJokes() {
-      try {
-        dispatch({ type: "JOKES_FETCH_REQUESTED" });
-        const resp = await fetch(
-          "https://official-joke-api.appspot.com/random_ten"
-        );
-        const data = await resp.json();
-        dispatch({ type: "JOKES_FETCH_SUCCESS", payload: data });
-      } catch (err) {
-        dispatch({ type: "JOKES_FETCH_FAILED", payload: "Fetch jokes failed" });
-      }
-    }
-    fetchMyJokes();
+    dispatch(fetchMyJokes());
+    // fetchMyJokes();
   }, []);
   return <div>This is where joke lies </div>;
 }
