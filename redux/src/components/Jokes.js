d409950600bffafb39e2b0959c6d5c0c4ffbba42
PranishShresth <yunche64@gmail.com>
2021-08-30 15:25:38 +1000

    async fetch handling

--- redux/src/components/Jokes.js
+++ redux/src/components/Jokes.js
@@ -1,5 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 export default function Jokes() {
-  return <div>This is where joke lies</div>;
+  const jokes = useSelector((state) => state.jokes);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    async function fetchMyJokes() {
+      try {
+        dispatch({ type: "JOKES_FETCH_REQUESTED" });
+        const resp = await fetch(
+          "https://official-joke-api.appspot.com/random_ten"
+        );
+        const data = await resp.json();
+        dispatch({ type: "JOKES_FETCH_SUCCESS", payload: data });
+      } catch (err) {
+        dispatch({ type: "JOKES_FETCH_FAILED", payload: "Fetch jokes failed" });
+      }
+    }
+    fetchMyJokes();
+  }, []);
+  return <div>This is where joke lies </div>;
 }
