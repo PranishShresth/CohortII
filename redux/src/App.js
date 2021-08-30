4e1331cffb69ba25e5bdd34d5626b8153c555c29
PranishShresth <yunche64@gmail.com>
2021-08-30 14:56:04 +1000

    useSelector and useDispatch

--- redux/src/App.js
+++ redux/src/App.js
@@ -1,7 +1,29 @@
 import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
 
 function App() {
-  return <div className="App"></div>;
+  const dispatch = useDispatch();
+  const store = useSelector((state) => state);
+  return (
+    <div className="App">
+      <h1>Bank App</h1>
+      <pre>{JSON.stringify(store, null, 2)}</pre>
+      <button
+        onClick={() => {
+          dispatch({ type: "WITHDRAW_MONEY" });
+        }}
+      >
+        Withdraw 10$
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: "DEPOSIT_MONEY" });
+        }}
+      >
+        Deposit 10$
+      </button>
+    </div>
+  );
 }
 
 export default App;
