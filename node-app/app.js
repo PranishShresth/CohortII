25242d8b17dfe796793788fb28b33393bb5d76ee
PranishShresth <yunche64@gmail.com>
2021-09-02 14:04:59 +1000

    seedint the database

--- node-app/app.js
+++ node-app/app.js
@@ -3,16 +3,35 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const path = require("path");
 const mongoose = require("mongoose");
+const Todo = require("./todo");
 const fs = require("fs");
 require("dotenv").config();
 
 app.use(express.json());
 
-app.get("/", function (req, res) {
-  fs.readFile("./package.json", { encoding: "utf-8" }, function (err, data) {
-    res.send(data);
-  });
+// GET, POST, PUT ,DELETE,
+
+app.get("/", async function (req, res) {
+  const todos = await Todo.find({});
+  res.send(todos);
 });
+//seeding the database
+// const todos = [
+//   { task: "Do the laundry 1" },
+//   { task: "Do the chores 1" },
+//   { task: "Go shopping 1" },
+// ];
+
+// Todo.create(todos)
+// todos.forEach((todo) => {
+//   const a = new Todo(todo);
+//   a.save();
+// });
+// app.get("/", function (req, res) {
+//   fs.readFile("./package.json", { encoding: "utf-8" }, function (err, data) {
+//     res.send(data);
+//   });
+// });
 // console.log(process.env.MONGO_URI);
 
 app.listen(PORT, () => {
---
+++ node-app/todo.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const TodoSchema = new Schema({
+  task: String,
+});
+
+const Todo = mongoose.model("Todo", TodoSchema);
+module.exports = Todo;
