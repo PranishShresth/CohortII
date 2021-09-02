27fba44e0828d4d9f0a605a6e77c09829e1dffd6
PranishShresth <yunche64@gmail.com>
2021-09-02 14:16:21 +1000

    find by query

--- node-app/app.js
+++ node-app/app.js
@@ -4,16 +4,23 @@ const PORT = process.env.PORT || 5000;
 const path = require("path");
 const mongoose = require("mongoose");
 const Todo = require("./todo");
+const logger = require("morgan");
 const fs = require("fs");
 require("dotenv").config();
 
 app.use(express.json());
+app.use(logger("dev"));
 
 // GET, POST, PUT ,DELETE,
 
 app.get("/", async function (req, res) {
   const todos = await Todo.find({});
   res.send(todos);
+});
+
+app.put("/todos/:todoId", async function (req, res) {
+  const todo = await Todo.find({ task: "Do the chores 12" });
+  res.send(todo);
 });
 //seeding the database
 // const todos = [
